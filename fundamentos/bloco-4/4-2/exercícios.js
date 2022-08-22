61f4ac62f5524c6309db3f091fd5425628744894
wesleymktd <wesleymktd@gmail.com>
2022-08-22 19:30:50 -0300

    terminei

--- fundamentos/bloco-4/4-2/exercícios.js
+++ fundamentos/bloco-4/4-2/exercícios.js
@@ -26,3 +26,90 @@ for (let name of names) {
 }
 */
 
+/*
+function dice () { 
+  return Math.ceil(Math.random() * 6);
+}
+let d1 = dice();
+let d2 = dice();
+let cont = 0;
+while (d1 === d2) {
+  d2 = dice();
+  cont += 1 
+}
+*/
+
+//exercícios práticos
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*1 - for (let number of numbers) {
+  console.log(number);
+}*/
+
+/*2 - let resultadoSoma = 0
+for (let index = 0; index < numbers.length; index += 1) {
+  resultadoSoma = resultadoSoma + numbers[index];
+}
+console.log(resultadoSoma);
+*/
+
+/*let resultadoSoma = 0
+
+for (let index = 0; index < numbers.length; index += 1) {
+  resultadoSoma = resultadoSoma + numbers[index];
+}
+let mediaAritmetica = resultadoSoma / numbers.length
+console.log(mediaAritmetica);*/
+
+/*let resultadoSoma = 0
+
+for (let index = 0; index < numbers.length; index += 1) {
+  resultadoSoma = resultadoSoma + numbers[index];
+}
+let mediaAritmetica = resultadoSoma / numbers.length
+if (mediaAritmetica > 20) {
+  console.log('valor maior que 20')
+} else {
+  console.log('valor menor ou igual a 20')
+}
+*/
+/*
+let maiorValor = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > maiorValor) {
+    maiorValor = numbers[index];
+  }  
+}
+console.log(maiorValor);
+*/
+/*qValoresImpares = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    qValoresImpares += 1
+  }
+}
+  console.log(qValoresImpares);
+*/
+
+// exercício 7 
+/*let menorValor = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] < menorValor) {
+    menorValor = numbers[index];
+  }  
+}
+console.log(menorValor);
+*/
+//exercício 8 
+/*
+let numeros =[];
+
+for (let index = 1 ; index <= 25 ; index += 1) {
+  let divide = index / 2;
+  numeros.push(divide);
+}
+console.log(numeros);
+*/
+faltou apenas o bonus para concluir
---
+++ fundamentos/bloco-4/4-3/fixandoAorebd.js
@@ -0,0 +1,13 @@
+/* criar uma variável que vai guardar o resultado da soma onde ela inicializará com 0
+criar uma estrutura de repetição, onde vai percorrer todos os números de 1 a 100
+criar uma lógica onde irá somar o termo atual com o número da sequencia
+fechar o looping
+escrever um comando para que o resultado seja impresso na tela*/
+/*let resultadoSoma = 0;
+for (let index = 1; index <= 100; index += 1) {
+    resultadoSoma += index;
+}
+console.log(resultadoSoma);*/
+
+
+   
