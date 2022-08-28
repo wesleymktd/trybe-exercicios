0c5d310a09ab87ff0388df821f021794bf3e04d1
wesleymktd <wesleymktd@gmail.com>
2022-08-28 16:38:04 -0300

    terminei p1

---
+++ fundamentos/bloco-5/5.2/script.js
@@ -0,0 +1,5 @@
+// arquivo script.js aqui tá mostrando a diferença do nextSibling e nextElementSibling
+
+ //console.log(document.getElementById('start').nextSibling) // nó
+
+ console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
