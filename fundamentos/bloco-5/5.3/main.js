62c7c6a38806437b163bfecc8544e5e8bc832dcb
wesleymktd <wesleymktd@gmail.com>
2022-08-30 03:41:56 -0300

    terminei p2

--- fundamentos/bloco-5/5.3/main.js
+++ fundamentos/bloco-5/5.3/main.js
@@ -8,20 +8,43 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
-
+   
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function handClick(event) {
+  const techElement = document.querySelector('tech');
+  techElement.classList.remove('tech');
+  techElement.classList.add('tech')
+  input.value = '';   
+}
+
+firstLi.addEventListener('click', handClick) //essa função vai escutar o que acontece dentro das <li>
+secondLi.addEventListener('click', handClick)
+thirdLi.addEventListener('click', handClick)
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+input.addEventListener('input', function(event) {
+  const classText = document.querySelector('tech');
+  classText.innerText = event.target.value
+})   
+  
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
-
+  myWebpage.addEventListener('dblclick', function() {
+    window.location.replace('https://blog.betrybe.com/');
+  })
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+  myWebpage.addEventListener('mouseover', function(event){
+    event.target.style.color = 'red';
+  })
 
+  myWebpage.addEventListener('mouseout', function(event){
+    event.target.style.color = 'unset';
+  })
 // Segue abaixo um exemplo do uso de event.target:
 
 
