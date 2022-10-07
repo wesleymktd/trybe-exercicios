a3377de29762ca85e5ca5700c4468fe0ba8696ea
wesleymktd <wesleymktd@gmail.com>
2022-10-07 18:36:57 -0300

    ok

--- front-end/1/1.1/ex1.js
+++ front-end/1/1.1/ex1.js
@@ -22,18 +22,44 @@
 //   A mensagem deve ser impressa no console depois de no máximo 5 segundos. 
 //   Dica: utilize a função messageDelay para gerar o tempo de espera necessário
 
-  const messageDelay = () => Math.floor(Math.random() * 5000);
+//   const messageDelay = () => Math.floor(Math.random() * 5000);
+
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// };
+
+// // crie a função sendMarsTemperature abaixo
+// const sendMarsTemperature = () => {
+  
+//   setTimeout(() => console.log(`A temperatura de Marte é: ${getMarsTemperature()} graus celsius`), messageDelay())   
+// }
+// sendMarsTemperature(); // Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo
+
+//---------------------------gi----
+
+
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
 
 const getMarsTemperature = () => {
   const maxTemperature = 58;
   return Math.floor(Math.random() * maxTemperature);
 };
 
-// crie a função sendMarsTemperature abaixo
-const sendMarsTemperature = () => {
-  
-  setTimeout(() => console.log(`A temperatura de Marte é: ${getMarsTemperature()} graus celsius`), messageDelay())   
-}
-sendMarsTemperature(); // Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
 
-//---------------------------gi----
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`Atualmente está ${toFahrenheit(temperature)}ºF em Marte`);
+
+const greet = (temperature) =>
+  console.log(`Olá! Curiosity aqui. Nesse momento está ${temperature}ºC em Marte`);
+
+const sendMarsTemperature = () = {
+  const tempFarenheit = temperatureInFahrenheit()  
+  setTimeout(() => console.log(`Atualmente está ${tempFarenheit} em Marte`), messageDelay())
+}
+
+sendMarsTemperature(temperatureInFahrenheit); // Imprime "Atualmente está 46.4ºF em Marte", por exemplo
+
+sendMarsTemperature(greet); // Imprime "Olá! Curiosity aqui. Nesse momento são 36ºC em Marte", por exemplo
