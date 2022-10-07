63353b4aff4ce1eb877dfde8c1d7df20ecef69f7
wesleymktd <wesleymktd@gmail.com>
2022-10-07 18:27:37 -0300

    ok

---
+++ front-end/1/1.2/fetch.js
@@ -0,0 +1,94 @@
+// exemplo sem colocar a função fetch
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   console.log(fetch(url));
+// }
+
+// fetchJoke();
+// ------------------------------------------
+// exemplo colocando a função fetch a gente trata a requisição para a mensagem chegar
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value));
+// }
+
+// fetchJoke();
+//-----------------------------------------------
+//  agora adicionando o .catch() ao código que vai tratar se tivr erro
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+// }
+
+// fetchJoke();
+
+// Algo deu errado :( 
+// TypeError: Only absolute URLs are supported
+//------------------------------
+// utilizando a função async sozinha ela faz a mesma coisa do que a anterior
+// const fetch = require('node-fetch');
+
+// const fetchJoke = async () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+// }
+
+// fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
+//------------------------------------------------------
+//async em conjunto com await aí sim faz a função esperar até o final da sua execução
+// const fetch = require('node-fetch');
+
+// const fetchJoke = async () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   const result = await fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => data.value)
+//     .catch((error) => `Algo deu errado :( \n${error}`);
+  
+//   console.log(result);
+// };
+
+// fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
+
+// refatorando o than / catch pelo try/catch
+
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch(error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
+}
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
