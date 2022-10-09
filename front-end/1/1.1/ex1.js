bea6b1beb165453a0aa29fe59bf8b92cbeb367fd
wesleymktd <wesleymktd@gmail.com>
2022-10-09 19:32:10 -0300

    terminei mais

--- front-end/1/1.1/ex1.js
+++ front-end/1/1.1/ex1.js
@@ -36,30 +36,90 @@
 // }
 // sendMarsTemperature(); // Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo
 
-//---------------------------gi----
+//------------------------------------------------------
 
+// - Agora que você fez a função que envia a temperatura de Marte, vamos utilizar essa temperatura
+// para realizar outras operações. No código abaixo, temos as funções temperatureInFahrenheit e greet.
+// Elas irão utilizar a temperatura obtida pela função getMarsTemperature para realizar nossas
+// operações. Escreva a função sendMarsTemperature de forma que:
+// A função sendMarsTemperature receba uma callback;
+// Dica: Essa callback será uma de nossas funções: temperatureInFahrenheit ou greet;
 
+// A função sendMarsTemperature execute essa callback depois de no máximo 5 segundos.
+// Dica: Lembre-se de nossa função messageDelay para gerar o tempo de espera necessário.
 
-const messageDelay = () => Math.floor(Math.random() * 5000);
+// const messageDelay = () => Math.floor(Math.random() * 5000);
 
-const getMarsTemperature = () => {
-  const maxTemperature = 58;
-  return Math.floor(Math.random() * maxTemperature);
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// };
+
+// const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+
+// const temperatureInFahrenheit = (temperature) =>
+//   console.log(`Atualmente está ${toFahrenheit(temperature)}ºF em Marte`);
+
+// const greet = (temperature) =>
+//   console.log(`Olá! Curiosity aqui. Nesse momento está ${temperature}ºC em Marte`);
+
+// const sendMarsTemperature = (onSucess) => {
+//   const tempAtual = getMarsTemperature();
+//   setTimeout(() => onSucess(tempAtual), messageDelay());
+// }
+
+// sendMarsTemperature(temperatureInFahrenheit); // Imprime "Atualmente está 46.4ºF em Marte", por exemplo
+
+// sendMarsTemperature(greet); // Imprime "Olá! Curiosity aqui. Nesse momento são 36ºC em Marte", por exemplo
+
+//--------------------------------------------------
+// - Por fim, o robô Curiosity tem uma taxa de sucesso de envio de mensagem de 60% devido ao fato 
+// de o robô já estar muito ocupado com outras operações. Logo, adicione na função sendMarsTemperature
+//  uma outra callback que contenha as ações a serem tomadas em caso de falha.
+// const messageDelay = () => Math.floor(Math.random() * 5000);
+
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// }
+
+// const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+
+// const temperatureInFahrenheit = (temperature) =>
+//   console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+// const greet = (temperature) =>
+//   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// const handleError = (errorReason) =>
+//   console.log(`Error getting temperature: ${errorReason}`);
+
+// // definição da função sendMarsTemperature...
+// const sendMarsTemperature = (onSucess, onError) => {
+//   const messageSusses = Math.random() <= 0.6;
+//   const tempAtual = getMarsTemperature();
+//   setTimeout(() => {
+//     if(messageSusses)  onSucess(tempAtual)
+//     else onError('Robot is busy'); 
+//   }, messageDelay()); 
+// }
+
+// // imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+// sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+// sendMarsTemperature(greet, handleError);
+
+//--------------------------------------------------
+
+// - A função uppercase recebe um parâmetro str e uma callback. Esta função transforma as letras
+//  de uma palavra em letras maiúsculas. Escreva um teste que verifique a chamada da função uppercase.
+//   Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
+
+const uppercase = (str, callback) => {
+  setTimeout(() => {
+    callback(str.toUpperCase());
+  }, 500);
 };
 
-const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
-
-const temperatureInFahrenheit = (temperature) =>
-  console.log(`Atualmente está ${toFahrenheit(temperature)}ºF em Marte`);
-
-const greet = (temperature) =>
-  console.log(`Olá! Curiosity aqui. Nesse momento está ${temperature}ºC em Marte`);
-
-const sendMarsTemperature = () = {
-  const tempFarenheit = temperatureInFahrenheit()  
-  setTimeout(() => console.log(`Atualmente está ${tempFarenheit} em Marte`), messageDelay())
-}
-
-sendMarsTemperature(temperatureInFahrenheit); // Imprime "Atualmente está 46.4ºF em Marte", por exemplo
-
-sendMarsTemperature(greet); // Imprime "Olá! Curiosity aqui. Nesse momento são 36ºC em Marte", por exemplo
+it
