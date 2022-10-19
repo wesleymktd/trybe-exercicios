f6137eb45ee704ad2c59804bde1346b02413c886
wesleymktd <wesleymktd@gmail.com>
2022-10-19 15:25:08 -0300

    terminei

---
+++ front-end/2/2.1/xablau/src/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+    <div>
+      <h1>
+        {'Wesley Alves Moreira'}
+      </h1>
+      <p>
+        {'Sou estudante de front-end na trybe determinado, responsável me desenvolvendo em Web'}
+      </p>
+      <h2>
+        {'Minhas Habilidades'}
+      </h2>
+      <ul>
+        <li>{'Git/Hub'}</li>
+        <li>{'JavaScript'}</li>
+        <li>{'CSS'}</li>
+        <li>{'React'}</li>
+      </ul>
+    </div>
+    );
+  }      
+}
+
+export default About;
