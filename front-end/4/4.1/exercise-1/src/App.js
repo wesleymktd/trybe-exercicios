8e2d133d4e880cdcf0802824329ed998b0b4110a
wesleymktd <wesleymktd@gmail.com>
2022-10-31 11:24:24 -0300

    terminei

---
+++ front-end/4/4.1/exercise-1/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.css';
+import PersonDetails from './components/PersonDetails';
+
+ class App extends Component {
+
+  render() {
+    return (
+      <PersonDetails />
+    );
+  }
+}
+
+export default App;
+
+
