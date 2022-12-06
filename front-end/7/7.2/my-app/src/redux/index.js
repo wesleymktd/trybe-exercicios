31da407352a84ce90f961b3389640b28fc83f74c
wesleymktd <wesleymktd@gmail.com>
2022-12-06 05:35:01 -0300

    top

---
+++ front-end/7/7.2/my-app/src/redux/index.js
@@ -0,0 +1,8 @@
+// ./src/redux/index.js
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
---
+++ front-end/7/7.2/my-app/src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+// ./src/reducers/index.js
+import { combineReducers } from 'redux';
+import counterReducer from './counterReducer';
+
+const rootReducer = combineReducers({ counterReducer });
+export default rootReducer;
