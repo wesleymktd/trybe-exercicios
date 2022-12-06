d4dad632838b5911709c86fd2c92bda67eeb3161
wesleymktd <wesleymktd@gmail.com>
2022-12-06 13:53:50 -0300

    doguinhos

---
+++ front-end/7/7.4/doguinhos-app/src/redux/index.js
@@ -0,0 +1,12 @@
+
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from 'redux-thunk';
+import dogReducer from './reducers/dogReducer'
+
+const store = createStore(
+  dogReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
